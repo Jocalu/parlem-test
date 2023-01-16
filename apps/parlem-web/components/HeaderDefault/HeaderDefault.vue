b58925c82dff35f4653afd7882aa12dc2672a62c
jocalu <jocalu87@gmail.com>
2023-01-16 18:18:36 +0100

    Add HeaderDefault Component

---
+++ apps/parlem-web/components/HeaderDefault/HeaderDefault.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header-default">
+    <div class="header-default__content">
+      <img
+        class="header-default__logo"
+        src="@/assets/svgs/logo.svg"
+        alt="Logo parlem"
+      />
+      <nuxt-link class="button--primary" to="/clients">
+        <span v-text="userAreaButtonText" />
+      </nuxt-link>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import { userAreaButtonText } from "../../locales/ca.json";
+</script>
+
+<style lang="scss" src="./HeaderDefault.scss"></style>
---
+++ apps/parlem-web/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="main">
+    <header-default>
+      <h1>Hola mundo</h1>
+    </header-default>
+  </main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss">
+.main {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+</style>
