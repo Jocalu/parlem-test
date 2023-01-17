949c5ce3321648e8b4fe3db5b56393e01c19556f
jocalu <jocalu87@gmail.com>
2023-01-17 09:38:23 +0100

    Update UserAreaProducts

--- apps/parlem-web/pages/user-area/products/index.vue
+++ apps/parlem-web/pages/user-area/products/index.vue
@@ -6,13 +6,22 @@
     <p class="mt-2" v-text="userAreaProductsText" />
 
     <div class="user-area mt-6">
-      <div class="user-area__products-wrapper">
+      <div class="user-area__data-wrapper">
         <subtitle-underlined :text="adsl" />
 
-        <div class="user-area__products px-4"></div>
+        <div class="user-area__data px-4 mb-4">
+          <text-labeled :label="userAreaProductsNameText" text="Parlem" />
+          <text-labeled :label="userAreaProductsSoldAtText" text="Parlem" />
+        </div>
+
         <subtitle-underlined :text="mobileLines" />
 
-        <div class="user-area__products px-4"></div>
+        <div class="user-area__data px-4">
+          <text-labeled :label="userAreaProductsNameText" text="Parlem" />
+          <text-labeled :label="userAreaProductsSoldAtText" text="Parlem" />
+        </div>
+
+        <div class="user-area__data px-4"></div>
       </div>
     </div>
   </section>
@@ -24,7 +33,7 @@ import {
   userAreaProductsText,
   mobileLines,
   adsl,
+  userAreaProductsSoldAtText,
+  userAreaProductsNameText,
 } from "../../../locales/ca.json";
 </script>
-
-<style lang="scss" src="./UserAreaProducts.scss"></style>
