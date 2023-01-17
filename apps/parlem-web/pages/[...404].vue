241004abbdc66e775881f05308a9420e2198e2e2
jocalu <jocalu87@gmail.com>
2023-01-17 12:25:02 +0100

    Add not found page

---
+++ apps/parlem-web/pages/[...404].vue
@@ -0,0 +1,10 @@
+<template>
+	<section class="container-responsive">
+		<h2 class="mt-4" v-text="pageNotFound" />
+		<p class="mt-2" v-text="pageNotFoundText" />
+	</section>
+</template>
+
+<script lang="ts" setup>
+import { pageNotFound, pageNotFoundText } from '~~/locales/ca.json'
+</script>
