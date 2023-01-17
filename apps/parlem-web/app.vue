07a641206b9faa2cf96171ab58f1e738651a3e16
jocalu <jocalu87@gmail.com>
2023-01-17 11:49:52 +0100

    Add meta title

--- apps/parlem-web/app.vue
+++ apps/parlem-web/app.vue
@@ -1,5 +1,18 @@
 <template>
-  <nuxt-layout>
-    <nuxt-page />
-  </nuxt-layout>
+	<nuxt-layout>
+		<nuxt-page />
+	</nuxt-layout>
 </template>
+
+<script lang="ts" setup>
+useHead({
+	title: 'Parlem test',
+	meta: [
+		{
+			hid: 'description',
+			name: 'description',
+			content: 'Parlem test',
+		},
+	],
+})
+</script>
